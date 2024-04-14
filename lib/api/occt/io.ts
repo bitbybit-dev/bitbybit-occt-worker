8aa1c11957c859ff49920564753d7ade29e47ca7
Matas Ubarevicius <matas@bitbybit.dev>
2024-04-14 13:22:15 +0300

    for backwards compatibility split io methods to the ones that return values and the ones that dont

--- lib/api/occt/io.ts
+++ lib/api/occt/io.ts
@@ -15,7 +15,22 @@ export class OCCTIO {
      * @shortname save step
      * @drawable false
      */
-    saveShapeSTEP(inputs: Inputs.OCCT.SaveStepDto<Inputs.OCCT.TopoDSShapePointer>): Promise<string> {
+    async saveShapeSTEP(inputs: Inputs.OCCT.SaveStepDto<Inputs.OCCT.TopoDSShapePointer>): Promise<void> {
+        this.saveSTEP(inputs);
+    }
+
+    /**
+     * Saves the step file and returns the text value
+     * @param inputs STEP filename and shape to be saved
+     * @group io
+     * @shortname save step and return
+     * @drawable false
+     */
+    async saveShapeSTEPAndReturn(inputs: Inputs.OCCT.SaveStepDto<Inputs.OCCT.TopoDSShapePointer>): Promise<string> {
+        return this.saveSTEP(inputs);
+    }
+
+    private saveSTEP(inputs: Inputs.OCCT.SaveStepDto<Inputs.OCCT.TopoDSShapePointer>): Promise<string> {
         return this.occWorkerManager.genericCallToWorkerPromise("io.saveShapeSTEP", inputs).then(s => {
             if (inputs.tryDownload && document) {
                 const blob = new Blob([s], { type: "text/plain" });
