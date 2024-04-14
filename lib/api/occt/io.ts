d90d3605fb7347723e86ba9547ba1a9f79a489ce
Matas Ubarevicius <matas@bitbybit.dev>
2024-04-14 13:08:42 +0300

    try download and binary implemented for saving files

--- lib/api/occt/io.ts
+++ lib/api/occt/io.ts
@@ -15,19 +15,22 @@ export class OCCTIO {
      * @shortname save step
      * @drawable false
      */
-    saveShapeSTEP(inputs: Inputs.OCCT.SaveStepDto<Inputs.OCCT.TopoDSShapePointer>): Promise<void> {
+    saveShapeSTEP(inputs: Inputs.OCCT.SaveStepDto<Inputs.OCCT.TopoDSShapePointer>): Promise<string> {
         return this.occWorkerManager.genericCallToWorkerPromise("io.saveShapeSTEP", inputs).then(s => {
-            const blob = new Blob([s], { type: "text/plain" });
-            const blobUrl = URL.createObjectURL(blob);
+            if (inputs.tryDownload && document) {
+                const blob = new Blob([s], { type: "text/plain" });
+                const blobUrl = URL.createObjectURL(blob);
 
-            const fileName = inputs.fileName ? inputs.fileName : "bitbybit-dev.step";
+                const fileName = inputs.fileName ? inputs.fileName : "bitbybit-dev.step";
 
-            const fileLink = document.createElement("a");
-            fileLink.href = blobUrl;
-            fileLink.target = "_self";
-            fileLink.download = fileName;
-            fileLink.click();
-            fileLink.remove();
+                const fileLink = document.createElement("a");
+                fileLink.href = blobUrl;
+                fileLink.target = "_self";
+                fileLink.download = fileName;
+                fileLink.click();
+                fileLink.remove();
+            }
+            return s;
         });
     }
 
