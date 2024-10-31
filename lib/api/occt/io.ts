964f70800f12dd9cc4c838ba1e07cd507a65eb41
Matas Ubarevicius <matas@bitbybit.dev>
2024-10-31 08:21:41 +0200

    started using native occt STL writer to save STL files

--- lib/api/occt/io.ts
+++ lib/api/occt/io.ts
@@ -30,6 +30,28 @@ export class OCCTIO {
         return this.saveSTEP(inputs);
     }
 
+    /**
+     * Saves the stl file
+     * @param inputs STL filename and shape to be saved
+     * @group io
+     * @shortname save stl
+     * @drawable false
+     */
+    async saveShapeStl(inputs: Inputs.OCCT.SaveStlDto<Inputs.OCCT.TopoDSShapePointer>): Promise<void> {
+        this.saveStl(inputs);
+    }
+
+    /**
+     * Saves the stl file and returns
+     * @param inputs STL filename and shape to be saved
+     * @group io
+     * @shortname save stl return
+     * @drawable false
+     */
+    async saveShapeStlAndReturn(inputs: Inputs.OCCT.SaveStlDto<Inputs.OCCT.TopoDSShapePointer>): Promise<string> {
+        return this.saveStl(inputs);
+    }
+
     private saveSTEP(inputs: Inputs.OCCT.SaveStepDto<Inputs.OCCT.TopoDSShapePointer>): Promise<string> {
         return this.occWorkerManager.genericCallToWorkerPromise("io.saveShapeSTEP", inputs).then(s => {
             if (inputs.tryDownload && document) {
@@ -37,6 +59,25 @@ export class OCCTIO {
                 const blobUrl = URL.createObjectURL(blob);
 
                 const fileName = inputs.fileName ? inputs.fileName : "bitbybit-dev.step";
+
+                const fileLink = document.createElement("a");
+                fileLink.href = blobUrl;
+                fileLink.target = "_self";
+                fileLink.download = fileName;
+                fileLink.click();
+                fileLink.remove();
+            }
+            return s;
+        });
+    }
+
+    private saveStl(inputs: Inputs.OCCT.SaveStlDto<Inputs.OCCT.TopoDSShapePointer>): Promise<string> {
+        return this.occWorkerManager.genericCallToWorkerPromise("io.saveShapeStl", inputs).then(s => {
+            if (inputs.tryDownload && document) {
+                const blob = new Blob([s], { type: "application/stl" });
+                const blobUrl = URL.createObjectURL(blob);
+
+                const fileName = inputs.fileName ? inputs.fileName : "bitbybit-dev.stl";
 
                 const fileLink = document.createElement("a");
                 fileLink.href = blobUrl;
